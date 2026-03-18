4286695b03b04d2f99b6ece4406091e4f5e2a169
Silas Santana <silassantanajr@gmail.com>
2026-03-18 11:30:00 +0000

    feat: add global error-handler middleware

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,4 +1,5 @@
 // Global error handler - normalises all thrown errors into JSON
+// Global error handler - normalises all thrown errors into JSON
 import { Request, Response, NextFunction } from 'express';
 
 export function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
